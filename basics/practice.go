b1980e7ce546036d766036bed85a4a66a4bdde21
diyorbekabdullayev <diyorbekabdullayev2003@gmail.com>
2025-05-18 14:13:31 +0500

    intermediate

---
+++ basics/practice.go
@@ -0,0 +1,60 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// const (
+// 	year         = 2025
+// 	month string = "May"
+// 	day          = 9
+// )
+
+// var currentYear = time.Now().Year()
+// var currentMonth = time.Now().Month()
+// var currentDay = time.Now().Day()
+
+// var remainder int = 13
+
+// var clientAge int
+
+// func calculateUserAge() {
+
+// 	// fmt.Print("Input your born year: ")
+// 	// fmt.Scan(&clientAge)
+
+// 	// clientAge = currentYear - clientAge
+// 	// fmt.Println("Your age is", clientAge)
+
+// 	fmt.Printf("Year: %v\nMonth: %v\nDay: %v\n", currentYear, currentMonth, currentDay)
+// 	fmt.Printf("Year: %v\nMonth: %v\nDay: %v\n", year, month, day)
+
+// 	remainder = remainder % 7
+// 	fmt.Println(remainder)
+
+// }
+
+// func testSomething() string {
+// 	testSomething := ""
+// 	return testSomething
+// }
+
+// func calculateRemainder() {
+
+// 	var userInput string
+
+// 	fmt.Print("Input number: ")
+// 	fmt.Scan(&userInput)
+
+// 	var result string
+
+// 	for _, v := range userInput {
+
+//           digit := v - '0' // convert rune to digit
+// 		if digit%2 == 0 {
+// 			result += string(v)
+// 		}
+// 	}
+// 	fmt.Println(result)
+// }
