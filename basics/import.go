78c3fe802ab6e5750c2ba646ace3eec0872bdaf2
diyorbekabdullayev <diyorbekabdullayev2003@gmail.com>
2025-05-18 14:13:02 +0500

    intermediate

---
+++ basics/import.go
@@ -0,0 +1,73 @@
+package main
+
+// // import "fmt"
+
+// // import "fmt"
+
+// type Calculater struct {
+// 	Name   string
+// 	Age    int
+// 	UserID int
+// }
+
+// func calculater() *Calculater {
+// 	calculater := Calculater{
+// 		Name:   "",
+// 		Age:    0,
+// 		UserID: 0,
+// 	}
+// 	return &calculater
+// }
+
+// func main() {
+// 	// value := test()
+// 	// value = "Ikrom"
+// 	// fmt.Println(value)
+
+// 	// myStruct := calculater()
+
+// 	// myStruct.Name = "Iskandar"
+// 	// fmt.Println(myStruct)
+
+// 	// calculateRemainder()
+
+// 	// var rows = 11
+
+// 	// for i := 1; i <= rows; i++ {
+
+// 	// 	for j := 1; j <= rows-i+13; j++ {
+// 	// 		fmt.Print(" ")
+// 	// 	}
+
+// 	// 	for k := 1; k <= 2*i-1; k++ {
+// 	// 		fmt.Print("*")
+// 	// 	}
+// 	// 	// for s := 1; s <= 2*i-1; s++ {
+// 	// 	// 	fmt.Print("A")
+// 	// 	// }
+
+// 	// 	fmt.Println()
+// 	// }
+
+// 	// var matrix = [3][3]int{
+// 	// 	{1, 2, 3},
+// 	// 	{4, 5, 6},
+// 	// 	{7, 8, 9},
+// 	// }
+
+// 	// fmt.Println(matrix[1][1])
+
+// 	// var mappie = make(map[int]string, 5)
+
+// 	// mappie[1] = "Hello!"
+// 	// mappie[2] = "Hellu!"
+// 	// mappie[3] = "Helli!"
+// 	// mappie[4] = "Helle!"
+// 	// mappie[5] = "Hella!"
+// 	// mappie[6] = "Helloo!"
+// 	// mappie[7] = "Hellou!"
+
+// 	// for k, v := range mappie {
+// 	// 	fmt.Println(k, ":", v)
+// 	// }
+// }
